f423c843fbf4271d3c88c7491a818a956c90227d
Cassidy <cassidy.pignatello@gmail.com>
2020-06-07 12:32:55 +0700

    adds Tailwind support

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Facts About Chuck</h1>
+const App = () => (
+  <div className="max-w-md mx-auto flex p-6 bg-gray-100 mt-10 rounded-lg shadow-xl">
+    <div className="ml-6 pt-1">
+      <header>
+        <h1 className="text-2xl text-blue-700 leading-tight">
+          Facts About Chuck
+        </h1>
       </header>
+      <p className="text-base text-gray-700 leading-normal">
+        Building apps together
+      </p>
     </div>
-  );
-}
+  </div>
+);
 
 export default App;
